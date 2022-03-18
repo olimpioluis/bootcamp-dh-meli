4276414cd7c932dc61dc4c4d59f576ec40b4fc09
Luis Olimpio <luis.olimpio@mercadolivre.com>
2022-03-18 18:14:43 -0300

    tweaks in previous exercises

--- modulo03/spring-I/exercicio01/src/main/java/br/com/meli/exercicio01/controllers/ConvertNumbersToRomanControllerNumber.java
+++ modulo03/spring-I/exercicio01/src/main/java/br/com/meli/exercicio01/controllers/ConvertNumbersToRomanControllerNumber.java
@@ -14,20 +14,13 @@ public class ConvertNumbersToRomanControllerNumber {
 
     @GetMapping("/numbers")
     @ResponseBody
-    public String convertNumberToRomanNumber(@RequestParam String number) {
+    public String convertNumberToRomanNumber(@RequestParam Integer number) {
         if (number != null) {
-                int numberConverted;
-            try{
-                numberConverted = Integer.parseInt(number);
-            } catch(NumberFormatException e) {
-                return "O valor informado é inválido";
-            }
-
             StringBuilder roman = new StringBuilder();
 
             for(int i=0; i<VALUES.length; i++) {
-                while(numberConverted >= VALUES[i]) {
-                    numberConverted -= VALUES[i];
+                while(number >= VALUES[i]) {
+                    number -= VALUES[i];
                     roman.append(ROMANS[i]);
                 }
             }
